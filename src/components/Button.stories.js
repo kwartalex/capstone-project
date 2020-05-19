85a22e7a3f01e6de8313ada686495910a56968bf
kwartalex <61711399+kwartalex@users.noreply.github.com>
2020-05-19 11:36:13 +0200

    Update storybook

--- src/components/Button.stories.js
+++ src/components/Button.stories.js
@@ -1,27 +1,16 @@
 import React from 'react';
 import Button from './Button'
-import { action, text } from '@storybook/addon-actions';
-import { withKnobs } from '@storybook/addon-knobs'
+import { action } from '@storybook/addon-actions';
 
 export default {
   title: 'Button',
   component: Button,
-  decorators: [withKnobs],
 };
 
-export const BtnBasic = () => (
-  <Button 
-  text="Short Text" 
-  onClick={action('onClick')}
-  />
+export const BtnUnclicked = () => (
+  <Button />
 );
 
-export const BtnBasicLongText = () => (
-  <Button onClick={action('onClick')}>Long Text in Button Component</Button> 
-);
-
-export const BtnActive = () => (
-  <Button 
-  text="Short Text" 
-  onClick={action('onClick')}/>
+export const BtnClicked = () => (
+  <Button active={action('onClick')}></Button> 
 );
