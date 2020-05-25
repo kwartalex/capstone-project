bee24b0dc6e26f64076ee4c94be4dab1f93c505d
kwartalex <61711399+kwartalex@users.noreply.github.com>
2020-05-25 17:59:31 +0200

    Add picture api

--- src/pages/HappyPic.js
+++ src/pages/HappyPic.js
@@ -1,30 +1,39 @@
 import React from 'react'
 import styled from 'styled-components/macro'
-import happypic from '../images/dachshund.png'
 
 export default function PicRotation () {
     return (
             <>
+            <StyledParagraphTop>HAPPY PICTURE OF THE DAY!</StyledParagraphTop>
             <StyledImage>
                 <img              
-                src={happypic} alt="Happy pucture of the day" />
+                src="https://source.unsplash.com/featured/?puppy" alt="Happy picture of the day" />
             </StyledImage>
-            <StyledParagraph>Bitterness keeps you from flying!</StyledParagraph>
+            <StyledParagraphBottom>Bitterness keeps you from flying!</StyledParagraphBottom>
             </>
     )}
 
-    const StyledParagraph = styled.p `
+    const StyledParagraphTop = styled.p `
         font-weight: lighter;
         font-size: 24px;
+        margin-top: 140px; 
     `
 
+    const StyledParagraphBottom = styled.p `
+    font-weight: lighter;
+    font-size: 24px;
+`
+
     const StyledImage = styled.p `
-        
         overflow: hidden;
         border-radius: 50%;
         border: 2px solid var(--septenary);
         width: 240px;
-        margin-top: 160px;
+        margin-top: 20px;
         margin-bottom: 40px;
         margin-left: 70px; 
+
+        img {
+            height: 240px;
+        }
     `
